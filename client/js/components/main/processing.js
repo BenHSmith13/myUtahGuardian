8438b608aee7ea7f0c21d7c8b6efc635d9d9fe88
dittonjs <jditton.atomic@gmail.com>
2016-03-05 11:35:13 -0700

    adds basic analytics

--- client/js/components/main/processing.js
+++ client/js/components/main/processing.js
@@ -6,7 +6,7 @@ const flagWords = [
   "victorias secret",
   "flixx",
   "asianbeauties",
-  "porn",
+  "pawn",
   "alcohol",
   "tobacco",
   "smokes",
@@ -17,58 +17,43 @@ export default class Processor {
   static processTransaction(transaction, uid){
     var fireRef = new Firebase("https://myutahguardian.firebaseio.com/transactions/"+uid);
     var problemCounts = {};
-    fireRef.on("value", (snapshot)=>{
-      var transactions = snapshot.val();
-      _.each(transactions, (trans)=>{
-        var descriptionWords = trans.description.split(" ");
-        _.each(descriptionWords, (dword)=>{
-          _.each(flagWords, (word)=>{
-            if(new Levenshtein(word, dword).distance < dword.length / 3){
-              if(problemCounts[word]){
-                problemCounts[word].count++;
-                problemCounts[word].inputWords.push(dword);
-              } else {
-                problemCounts[word] = {
-                  count:  1,
-                  inputWords: [dword],
-                  ids: [dword]
-                }
-              }
+    var descriptionWords = transaction.description.split(" ");
+    _.each(descriptionWords, (dword)=>{
+      _.each(flagWords, (word)=>{
+        if(new Levenshtein(word, dword).distance < dword.length / 3){
+          if(problemCounts[word]){
+            problemCounts[word].count++;
+            problemCounts[word].inputWords.push(dword);
+          } else {
+            problemCounts[word] = {
+              count:  1,
+              inputWords: [dword],
+              ids: [dword]
             }
-          });
-        });
-      });
-      var fbTransRef = new Firebase("https://myutahguardian.firebaseio.com/red_flags/")
-      _.each(problemCounts,(val, key)=>{
-        if(val.count >= 5){
-          var isInThisTransaction = false;
-          _.each(val.inputWords, word=>{
-            if(transaction.description.indexOf(word) > -1) isInThisTransaction = true;
-          });
-          if(isInThisTransaction){
-            fbTransRef.push({level: 4, uid, ...transaction});
           }
         }
-        else if(val.count > 3){
-          var isInThisTransaction = false;
-          _.each(val.inputWords, word=>{
-            if(transaction.description.indexOf(word) > -1) isInThisTransaction = true;
-          });
-          if(isInThisTransaction){
-            fbTransRef.push({level: 3, uid, ...transaction});
-          }
-        }
-        else if(val.count > 1){
-          var isInThisTransaction = false;
-          _.each(val.inputWords, word=>{
-            if(transaction.description.indexOf(word) > -1) isInThisTransaction = true;
-          });
-          if(isInThisTransaction){
-            fbTransRef.push({level: 2, uid, ...transaction});
-          }
-        }
-
       });
     });
+    var fbTransRef = new Firebase("https://myutahguardian.firebaseio.com/red_flags/");
+    console.log(problemCounts);
+    _.each(problemCounts,(val, key)=>{
+      if(val.count >= 1){
+        if(_.includes(["flixx", "asianbeauties"], key)){
+          //do four stuff
+          fbTransRef.push({level: 4, uid, ...transaction});
+        }
+        if(_.includes(["liquor", "victorias secret", "payday", "alcohol"], key)){
+          fbTransRef.push({level: 3, uid, ...transaction});
+        }
+        if(_.includes(["cigar", "casino", "pawn"], key)){
+          fbTransRef.push({level: 2, uid, ...transaction});
+        }
+        if(_.includes(["tobacco", "smokes", "cigarrete"], key)){
+          fbTransRef.push({level: 1, uid, ...transaction});
+        }
+
+      }
+    });
   }
+
 }
