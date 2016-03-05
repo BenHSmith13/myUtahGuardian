bcb74b7749e3af8fecf364c65da761e8ceb78d2c
dittonjs <jditton.atomic@gmail.com>
2016-03-04 22:56:13 -0700

    test deploy

--- client/js/components/main/home.jsx
+++ client/js/components/main/home.jsx
@@ -1,7 +1,6 @@
 "use strict";
 
 import React         from 'react';
-import Firebase      from 'firebase';
 import SettingsStore from '../../stores/settings';
 
 
@@ -9,8 +8,6 @@ export default class Home extends React.Component{
 
   constructor(){
     super();
-    this.firebaseItemsUrl = `${SettingsStore.current().firebaseUrl}/items`;
-    this.firebaseRef = new Firebase(this.firebaseItemsUrl);
     this.state = {
       items: {},
       text: ""
@@ -18,42 +15,20 @@ export default class Home extends React.Component{
   }
 
   componentWillMount(){
-    // Firebase docs for 'on': https://www.firebase.com/docs/web/api/query/on.html
-    this.firebaseRef.on("child_added", function(dataSnapshot) {
-      this.state.items[dataSnapshot.key()] = dataSnapshot.val()
-      this.setState({ items: this.state.items });
-    }.bind(this));
-    this.firebaseRef.on("child_removed", function(dataSnapshot) {
-      delete this.state.items[dataSnapshot.key()];
-      this.setState({ items: this.state.items });
-    }.bind(this));
+
   }
 
   componentWillUnmount(){
-    this.firebaseRef.off();
   }
   
-  handleAdd(e){
-    e.preventDefault();
-    this.firebaseRef.push({
-      text: this.refs.newItem.value
-    });
-  }
-
-  handleDelete(e, id){
-    e.preventDefault();
-    var itemRef = new Firebase(`${this.firebaseItemsUrl}/${id}`);
-    itemRef.remove();
-  }
 
   render(){
     var items = _.map(this.state.items, (item, id) => {
       return <li key={id}>{item.text} <button onClick={(e) => {this.handleDelete(e, id);}}>Delete</button></li>;
     });
-    return(<div>
-      <input ref="newItem" type="text" />
-      <button onClick={(e) => {this.handleAdd(e);}} >Add</button>      
-      <ul>{items}</ul>
+    return(
+    <div className="container">
+      <div className="jumbotron"><h1>Hello America</h1></div>
     </div>);
   }
 };
