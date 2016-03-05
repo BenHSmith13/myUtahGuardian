4c80c52048cc2e0b096fedf48a3470838d4c9c24
dittonjs <jditton.atomic@gmail.com>
2016-03-05 04:47:30 -0700

    adds transactions

--- client/js/components/main/add_transactions.jsx
+++ client/js/components/main/add_transactions.jsx
@@ -19,13 +19,13 @@ export default class AddTransactions extends BaseComponent{
   getStyles(){
     return {
       container: {
-        marginTop: "70px"  
+        marginTop: "80px"  
       },
       transactionForm: {
         margin: "auto",
         width: "80%",
         padding: "20px",
-        boxShaddow: "0px 1px 1px 0px",
+        boxShadow: "0px 1px 1px 0px",
         borderRadius: "3px"
       }
     }
@@ -35,7 +35,26 @@ export default class AddTransactions extends BaseComponent{
     return(
       <div className="container" style={styles.container}>
         <div style={styles.transactionForm}>
-
+          <h4>Add Transaction</h4>
+          <form>
+            <div className="form-group">
+              <label for="amount">Amount (In Dollars)</label>
+              <input ref="amount" type="text" className="form-control" id="amount" placeholder="Amount..."/>
+            </div>
+            <div className="form-group">
+              <label className="radio-inline">
+                <input type="radio" name="transaction_type" value="income"/>Income
+              </label>
+              <label style={{marginLeft: "5px"}} className="radio-inline">
+                <input type="radio" name="transaction_type" value="expense"/>Expense
+              </label>
+            </div>
+            <div className="form-group">
+              <label for="description">Description</label>
+              <textarea ref="description" className="form-control" rows="3"></textarea>
+            </div>
+            <button onClick={(e)=>{this.login(e)}}className="btn btn-default">Login</button>
+          </form>
         </div>
       </div>
     );
