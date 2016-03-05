59915906297ad24bd7fb4f7f64ff9fdbebe43f54
dittonjs <jditton.atomic@gmail.com>
2016-03-05 12:59:04 -0700

    sets up deploy

--- client/js/components/main/red_flags.jsx
+++ client/js/components/main/red_flags.jsx
@@ -6,6 +6,7 @@ import ApplicationStore from '../../stores/application';
 import BaseComponent from  "../base_component";
 import UserStore     from "../../stores/user";
 import history       from "../../history";
+import firebase      from "firebase";
 export default class RedFlags extends BaseComponent{
 
   constructor(){
@@ -14,6 +15,9 @@ export default class RedFlags extends BaseComponent{
   }
 
   getState(){
+  }
+  componentDidMount(){
+    var redFlagFB = new Firebase("https://myutahguardian.firebaseio.com/red_flags");
   }
   getStyles(){
     return {
@@ -24,9 +28,30 @@ export default class RedFlags extends BaseComponent{
   }
   render(){
     var styles = this.getStyles();
+    // var storedData = _.sort(this.state.redFlags, (flag)=>(flag.))
+    // var data = _.map(this.state.redFlags, (data, key)=>{
+    //   return (
+    //     <tr key={key}>
+    //       <td>{i++}</td>
+    //       <td>{data.amount}</td>
+    //       <td>{data.is_income ? "true" : "false"}</td>
+    //       <td>{data.is_expense ? "true" : "false"}</td>
+    //       <td>{catagories[data.category]}</td>
+    //       <td>{data.description}</td>
+    //     </tr>
+    //   );
+    // });
     return(
       <div className="container" style={styles.container}>
-        <h1>RED FLAGS</h1>
+        <div style={styles.paper}>
+          <h3>Flagged Transactions</h3>
+          <table className="table">
+            <thead><tr><th>#</th>{headerTags}</tr></thead>
+            <tbody>
+              {data}
+            </tbody>
+          </table>
+        </div>
       </div>
     );
   }
